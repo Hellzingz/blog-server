fbddb08cc63aab52a13ceaab6c9ccd8ad79f03c4
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-09-11 15:30:59 +0700

    feat: Enhance posts API with CRUD and pagination.

--- routes/posts.mjs
+++ routes/posts.mjs
@@ -1,8 +1,20 @@
 import express from "express";
 const postRouter = express.Router();
-import { create, read } from "../controllers/posts.mjs";
+import { create, deleteById, read ,readById,update } from "../controllers/posts.mjs";
 
+//GET
 postRouter.get("/", read);
+postRouter.get("/:postId", readById);
+
+//POST
 postRouter.post("/", create);
+
+//PUT
+postRouter.put("/:postId", update);
+
+//DELETE
+postRouter.delete("/:postId", deleteById);
+
+
 
 export default postRouter;
