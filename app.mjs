c5c9017c06548aef69455a7ae20c248477c3d3e3
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-09-15 20:31:33 +0700

    refactor: Update app listening logic, modify readAllPosts query for simplicity, and clean up protectRoute middleware by removing unnecessary validations

--- app.mjs
+++ app.mjs
@@ -25,6 +25,6 @@ app.get("/", (req, res) => {
 // Optional: handle favicon request
 app.get("/favicon.ico", (req, res) => res.status(204).end());
 
-// ❌ ห้ามใช้ app.listen() บน Vercel
+app.listen(4000)
 // ✅ Export app
 export default (req, res) => app(req, res)
--- utils/db.mjs
+++ utils/db.mjs
@@ -1,11 +1,11 @@
-import pkg from "pg";
-const { Pool } = pkg;
 import dotenv from 'dotenv'
 dotenv.config();
+import pkg from "pg";
+const { Pool } = pkg;
+
 
 const connectionPool = new Pool({
   connectionString: process.env.POSTGRESQL_URI,
-  ssl: { rejectUnauthorized: false }
 });
 
 export default connectionPool;
